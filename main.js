e9b2244b547e54fa5a9b37a1ac07f19c79dfd42f
Christian-web-hype <christiancasano6@gmail.com>
2024-03-09 17:45:09 +0100

    niente

--- main.js
+++ main.js
@@ -18,19 +18,6 @@ watch.forEach(element => {
     observer.observe(element);
 });
 
-// contact me
-
-// let name = document.querySelector('#name');
-
-// let surname = document.querySelector('#surname');
-
-// let message = document.querySelector('#message')
-
-// let wa = $name.value + $surname.value + $message.value;
-
-// let mex = document.querySelector('#mex');
-
-// mex = wa.me/3484910086?text=${encodeURIComponent(wa)};
 
 let sendButton = document.querySelector('#mex');
 
@@ -42,8 +29,7 @@ sendButton.addEventListener('click', function () {
   let name = nameInput.value;
   let surname = surnameInput.value;
   let message = messageInput.value;
-  console.log('->>', nameInput.value);
-
+  
   let whatsappMessage = `Piacere sono ${name} ${surname}, ${message}`;
 
   let encodedMessage = encodeURIComponent(whatsappMessage);
